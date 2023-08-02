70573817f046ff551b0ac471d2dd4abeb21f71fc
marcbelle <43605854+marcbelle@users.noreply.github.com>
2023-08-02 11:51:24 +0100

    Remove NI for private pensions and over State pension age
    Added logic to handle calculations and displays for removing NI when over state pension age or income is from a private pension.

--- app/routing/0-0-1/routes.js
+++ app/routing/0-0-1/routes.js
@@ -89,29 +89,41 @@ module.exports = function(router) {
       
 
     } else {
-      response.redirect('/' + version + '/state-pension-age')
+      response.redirect('/' + version + '/income-type')
     }
        
+  })
+  router.post('/' + version + '/income-type', function(request, response) {
+
+    var checkMode = request.session.data['checkMode'];
+
+    if (request.session.data['income-type'] == "private pension"){ //if income type is private pension skip state pension age
+
+      if (checkMode) {
+        response.redirect('/' + version + '/check-answers')
+      } else {
+        response.redirect('/' + version + '/registered-blind')
+      }
+
+    } else { // it's job income, check age
+      response.redirect('/' + version + '/state-pension-age')
+    }
   })
   router.post('/' + version + '/state-pension-age', function(request, response) {
 
     var checkMode = request.session.data['checkMode'];
-    if (checkMode) {
-      response.redirect('/' + version + '/check-answers')
-    } else {
-      response.redirect('/' + version + '/registered-blind')
-    }
 
-  })
-  router.post('/' + version + '/registered-blind', function(request, response) {
+    if (request.session.data['income-type'] == "private pension"){ //if income type is private pension skip pension contributions
 
-    var checkMode = request.session.data['checkMode'];
-    if (checkMode) {
-      response.redirect('/' + version + '/check-answers')
-    } else {
+      if (checkMode) {
+        response.redirect('/' + version + '/check-answers')
+      } else {
+        response.redirect('/' + version + '/registered-blind')
+      }
+
+    } else { // it's job income, check pension contributions
       response.redirect('/' + version + '/pension-contributions')
     }
-    
   })
   router.post('/' + version + '/pension-contributions', function(request, response) {
 
@@ -130,6 +142,16 @@ module.exports = function(router) {
 
     request.session.data['yearlyPensionContributions'] = yearlyPensionContributions;
 
+
+    var checkMode = request.session.data['checkMode'];
+    if (checkMode) {
+      response.redirect('/' + version + '/check-answers')
+    } else {
+      response.redirect('/' + version + '/registered-blind')
+    }
+    
+  })
+  router.post('/' + version + '/registered-blind', function(request, response) {
 
     var checkMode = request.session.data['checkMode'];
     if (checkMode) {
@@ -250,7 +272,25 @@ module.exports = function(router) {
             if (yearlySalary <= threshold20 ) { // User is only on 20%
               console.log("20 %");
               basicRateTotal = taxableIncome * 0.2;
-              nationalInsuranceTotal = taxableIncome * 0.12;
+              
+              if (request.session.data['income-type'] == 'job') {
+                // check age
+                if (request.session.data['state-pension-age'] == 'No') {
+                  // User pays NI
+                  nationalInsuranceTotal = taxableIncome * 0.12;
+                  console.log('Pay the NI!');
+                } else {
+                  // No NI because of State Pension age
+                  nationalInsuranceTotal = 0;
+                  console.log('pension age');
+                }
+              } else {
+                // no NI because it is a private pension
+                nationalInsuranceTotal = 0;
+                console.log('private pension');
+              }
+
+
               console.log(basicRateTotal);
               console.log(nationalInsuranceTotal);
               request.session.data['basicRateTotal'] = basicRateTotal;
@@ -260,10 +300,27 @@ module.exports = function(router) {
             } else { // User is on 40%
               console.log("40 %");
               var basicRateTotal = (threshold20 - taxFreeAmount) * 0.2; // work out how much to do at 20%
-              var NI20 = (threshold20 - taxFreeAmount) * 0.12;
               var higherRateTotal = (yearlySalary - threshold20) * 0.4; // do whatever is left at 40%
-              var NI40 = (yearlySalary - threshold20) * 0.02;
-              nationalInsuranceTotal = NI20 + NI40;
+
+              if (request.session.data['income-type'] == 'job') {
+                // check age
+                if (request.session.data['state-pension-age'] == 'No') {
+                  // User pays NI
+                  var NI20 = (threshold20 - taxFreeAmount) * 0.12;
+                  var NI40 = (yearlySalary - threshold20) * 0.02;
+                  nationalInsuranceTotal = NI20 + NI40;
+                  console.log('Pay the NI!');
+                } else {
+                  // No NI because of State Pension age
+                  nationalInsuranceTotal = 0;
+                  console.log('pension age');
+                }
+              } else {
+                // no NI because it is a private pension
+                nationalInsuranceTotal = 0;
+                console.log('private pension');
+              }
+
 
               console.log(basicRateTotal);
               console.log(higherRateTotal);
